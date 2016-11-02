5764c90ed7ef230605b6b08c41a1e9ef5540cad0
Komeiji Satori <i@sakura.kim>
2016-11-02 12:37:47 +0800

    update object to array

--- examples.php
+++ examples.php
@@ -2,10 +2,11 @@
 include 'E.PHP';
 定义变量('地址','https://api.satori.moe/hitokoto.php');
 定义变量('返回',HTTP读文本(地址));
+定义变量('JSON',Json反序列化(返回));
 //调试输出(HTTP读文本('http://www.baidu.com'));
 //执行('cmd');
 //取文字摘要('23333');
-调试输出(返回);
+调试输出(JSON);
 //调试输出(格式化时间('Y年m月d日 H:i:s'));
 //调试输出(获取URL参数('test'));
 //调试输出(HTML标记过滤(获取URL参数('w')));
